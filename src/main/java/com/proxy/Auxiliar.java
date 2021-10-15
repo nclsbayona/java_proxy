2fe4c1069f0b5447868687e84e5515739151a061
nclsbayona <nbayonaord@gmail.com>
2021-10-14 20:37:55 -0500

    cambio a Apache

--- src/main/java/com/proxy/Auxiliar.java
+++ src/main/java/com/proxy/Auxiliar.java
@@ -7,6 +7,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 //Estas importaciones se realizan para poder realizar las peticiones HTTP
 import java.net.URL;
+//Esta usa el protocolo HTTP corriendo sobre una capa de ssl o tls
+import javax.net.ssl.HttpsURLConnection;
+//Esta usa el protocolo HTTP
 import java.net.HttpURLConnection;
 
 // Añadiendo el soporte para multi-hilos
@@ -30,14 +33,14 @@ public class Auxiliar extends Thread {
                 linea = lectura.readLine();
                 // Si la linea está vacia, no la meto en el mensaje y ya acabe, en caso
                 // contrario la añado al mensaje
-                if (linea.length() == 0)
+                if (linea == null || linea.length() == 0)
                     fin = true;
                 else
                     // Si el mensaje no tiene nada, únicamente inserto la linea, en caso contrario
                     // inserto un salto de linea y la linea
                     mensaje += (mensaje.length() > 0) ? "\n" + linea : linea;
             }
-            lectura.close();
+            lectura.close(); // Cierro el buffer de lectura actual
             // Separo la peticion para poder realizar diferentes operaciones con los datos
             String[] peticion = mensaje.split("\n");
             String[] nueva_peticion = new String[peticion.length + 2];
@@ -50,35 +53,44 @@ public class Auxiliar extends Thread {
             for (String s : nueva_peticion)
                 if (s.contains("User-Agent:"))
                     agent = s.split(" ")[2];
-            System.out.println();
             String url_web = nueva_peticion[1];
-            String host = nueva_peticion[3].split(" ")[1];
-            if (!url_web.contains(host))
-                url_web = "http://" + host + url_web;
+            // Por si viene especificado el puerto, no tener en cuenta el puerto
+            url_web = url_web.split(":")[0];
+            System.out.println("El url web que detecto es: " + url_web);
+            /*
+             * String host = nueva_peticion[3].split(" ")[1]; if (!url_web.contains(host))
+             * url_web = "http://" + host + url_web;
+             */
+            if (!url_web.contains("http://"))
+                url_web = "http://" + url_web;
             url_web.replace("https://", "http://");
             URL url = null;
             mensaje = "";
             HttpURLConnection connection = null;
             try {
+                System.out.println("El host aquí es: " + url_web);
                 url = new URL(url_web);
                 System.out.println("The used protocol is " + url.getProtocol().toString());
                 connection = (HttpURLConnection) (url.openConnection());
+                // Si se intenta hacer uso de https para la conexión, primero se intentara
+                // cifrar la misma, por ende se enviará primero un mensaje de CONNECT para
+                // buscar llevar a cabo un TLS Handshake y cifrar la conexión, en este caso no
+                // queremos esto así que si vemos un cCONNECT, lo modificaremos por GET
+                nueva_peticion[0] = nueva_peticion[0].replace("CONNECT", "GET");
                 connection.setRequestMethod(nueva_peticion[0]);
                 connection.setRequestProperty("User-Agent", agent);
                 int response_code = connection.getResponseCode();
                 System.out.println("Response code: " + response_code);
                 if (response_code == HttpURLConnection.HTTP_OK) {
-                    System.out.println("Aqui estoy");
                     lectura = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                     fin = false;
                     while (!fin) {
-                        System.out.println("Leyendo ando");
                         // Leo todas las lineas del mensaje que me envia el servidor como respuesta a la
                         // petición del cliente
                         linea = lectura.readLine();
                         // Si la linea está vacia, no la meto en el mensaje y ya acabe, en caso
                         // contrario la añado al mensaje
-                        if (linea.length() == 0)
+                        if (linea == null || linea.length() == 0)
                             fin = true;
                         else
                             // Si el mensaje no tiene nada, únicamente inserto la linea, en caso contrario
@@ -97,19 +109,6 @@ public class Auxiliar extends Thread {
                 System.out.println(e.getMessage());
                 // TODO Definir que hacer
             }
-            System.out.println("Peticion al servidor " + host + " especificamente a " + url_web);
-            /*
-             * Ejemplos GET http://myblog.com/hola/vpn HTTP/1.1 Host: myblog.com
-             * Proxy-Connection: keep-alive Upgrade-Insecure-Requests: 1 User-Agent:
-             * Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)
-             * Chrome/94.0.4606.71 Safari/537.36 OPR/80.0.4170.40 Accept:
-             * text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,
-             * image/apng;q=0.8,application/signed-exchange;v=b3;q=0.9 Accept-Encoding:
-             * gzip, deflate Accept-Language: en-US,en;q=0.9 y (HTTPS) CONNECT
-             * wiki.ubuntu.com:443 HTTP/1.1 User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux
-             * x86_64; rv:93.0) Gecko/20100101 Firefox/93.0 Proxy-Connection: keep-alive
-             * Connection: keep-alive Host: wiki.ubuntu.com:443
-             */
         } catch (Exception e) {
             System.out.println(e.getMessage());
             // TODO definir que hacer aca (Error con la lectura)
